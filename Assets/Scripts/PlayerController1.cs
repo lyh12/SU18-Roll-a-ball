e887ea8ba32b71a45716b539a4048a99ab6a6e45
Lydia Ha <lyh12@scarletmail.rutgers.edu>
2018-06-05 19:35:42 -0400

    more edits to playerscript

--- Assets/Scripts/PlayerController1.cs
+++ Assets/Scripts/PlayerController1.cs
@@ -6,6 +6,7 @@ public class PlayerController1 : MonoBehaviour {
 
 	public float speed;
 	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
 	private int count;
@@ -15,6 +16,7 @@ public class PlayerController1 : MonoBehaviour {
 		rb = GetComponent<Rigidbody> ();
 		count = 0;
 		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate ()
@@ -30,7 +32,8 @@ public class PlayerController1 : MonoBehaviour {
 
 	void onTriggerEnter(Collider other)
 	{
-		if (other.gameObject.CompareTag ("Pick Up")) {
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
 			other.gameObject.SetActive (false);
 			count = count + 1;
 			SetCountText ();
